1dcc463785a0d01bc5824b52de3075aa794e024e
Selfty <vojta.mach@gmail.com>
2018-02-12 13:09:16 +0100

    odstraneni url.site.admin

--- popcorn/urls.py
+++ popcorn/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     # url(r'^$', 'popcorn.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
     url(r'', include('serials.urls')),
 
 ]
