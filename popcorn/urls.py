6ec1d24a6f3a948b4ac8379cfffd2dcb6a2e8ab3
Selfty <vojta.mach@gmail.com>
2018-03-01 11:40:33 +0100

    propoj

--- popcorn/urls.py
+++ popcorn/urls.py
@@ -7,9 +7,8 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    (r'^googlebc7a06bd22316fbf\.html$', lambda r: HttpResponse("google-site-verification: googlebc7a06bd22316fbf.html", mimetype="text/plain")),
-    (r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: ", mimetype="text/plain")),
     url(r'', include('serials.urls')),
+    url(r'', include('webmaster_verification.urls')),
 
 ]
 
