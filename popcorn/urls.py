91c8128e1d71bdb3a92db2df2dff922e5044e30d
Selfty <vojta.mach@gmail.com>
2018-03-01 11:28:36 +0100

    populace, propojeni s googlem

--- popcorn/urls.py
+++ popcorn/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^googlebc7a06bd22316fbf\.html$', lambda r: HttpResponse("google-site-verification: googlebc7a06bd22316fbf.html", mimetype="text/plain")),
-    url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: ", mimetype="text/plain")),
+    (r'^googlebc7a06bd22316fbf\.html$', lambda r: HttpResponse("google-site-verification: googlebc7a06bd22316fbf.html", mimetype="text/plain")),
+    (r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: ", mimetype="text/plain")),
     url(r'', include('serials.urls')),
 
 ]
+
+
