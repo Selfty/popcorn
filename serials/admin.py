e42429e345785a7b0e9225b6d2475dc716ad5221
Selfty <vojta.mach@gmail.com>
2019-01-02 20:56:27 +0100

    GM fields

--- serials/admin.py
+++ serials/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Post, Serials
+from .models import Post, Serials, Topserials
 
-admin.site.register(Post)
-admin.site.register(Serials)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','serie','episode')
+    pass
+
+admin.site.register(Post, PostAdmin)
+
+class SerialsAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    pass
+
+admin.site.register(Topserials)
+
+admin.site.register(Serials, SerialsAdmin)
+
